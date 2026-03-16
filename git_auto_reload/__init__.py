da0b124d865cacf6201f571960753156cfd1698b
木之清泠 <clemooling@staringplanet.top>
2026-03-16 13:34:28 +0800

    feat: implement optional rest_api support

--- git_auto_reload/__init__.py
+++ git_auto_reload/__init__.py
@@ -1,11 +1,26 @@
+try:
+    import rest_api
+except (ImportError, ModuleNotFoundError):
+    rest_api = None  # ty: ignore[invalid-assignment]
 from mcdreforged.api.all import PluginServerInterface
 from git_auto_reload.command import register
 
 
 def on_load(s: PluginServerInterface, _):
+    global rest_api
     register(s)
-    s.logger.info("PluginDevHelper loaded.")
+    if not rest_api:
+        rest_api = s.get_plugin_instance("rest_api")
+    s.logger.info("GitAutoReload loaded.")
 
 
 def on_unload(s: PluginServerInterface):
-    s.logger.info("PluginDevHelper unloaded.")
+    s.logger.info("GitAutoReload unloaded.")
+
+
+@rest_api.webhook(
+    "git_auto_reload", summary="GitAutoReload debugger.", require_auth=True
+)
+async def on_debug_webhook(data: dict):
+    """Debug port. Return the data posted."""
+    return data
